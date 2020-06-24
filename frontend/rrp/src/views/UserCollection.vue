dd301bda3ef0161202ced3f386038aaa88112e35
YefengNiu <santece.rise@gmail.com>
2020-06-24 12:11:48 +0800

    Frontend: Keep working on user collection page

--- frontend/rrp/src/views/UserCollection.vue
+++ frontend/rrp/src/views/UserCollection.vue
@@ -1,39 +1,44 @@
 <template>
 	<div class="root">
-		<Header></Header>
+		<Header ref="topbar"></Header>
 
 		<!-- 404 page if there is no book_id in query -->
-		<!-- 		<main>
+		<main v-if="pageNotFound">
 			<NotFound></NotFound>
-		</main> -->
+		</main>
 
-		<main>
+		<main v-else>
 			<div class="user-dashboard">
 				<div class="top">
-					<div class="title">
-						Dashboard - Yefeng Niu
+					
+					<div class="title" v-if="isMyDashboard()">
+						My dashboard - {{ account.username }}
 					</div>
-					<button class="btn-default btn-style-white animation-fadein-top delay_04s">New Collection</button>
+					<div class="title" v-else>
+						User page - {{ account.username }}
+					</div>
+					
+					<button class="btn-default btn-style-white animation-fadein-top delay_04s" v-if="isMyDashboard()">New Collection</button>
 				</div>
 				<div class="content">
 					<div class="left">
 						<ul>
-							<li class="selected" onclick="window.location.replace('user_collection.html')">
+							<li class="selected">
 								<img src="../../public/icon/Collection.png">
 								<span>Collections</span>
 								<div>2</div>
 							</li>
-							<li onclick="window.location.replace('user_monthly_goal.html')">
+							<li>
 								<img src="../../public/icon/goal.png">
 								<span>Monthly Goal</span>
 								<div>1</div>
 							</li>
-							<li onclick="window.location.replace('user_read_history.html')">
+							<li>
 								<img src="../../public/icon/already-read.png">
 								<span>Read History</span>
 								<div>11</div>
 							</li>
-							<li onclick="window.location.replace('user_reviews.html')">
+							<li>
 								<img src="../../public/icon/my-reviews.png">
 								<span>My Reviews</span>
 								<div>2</div>
@@ -154,16 +159,76 @@
 </template>
 
 <script>
+	import API_URL from '../serviceAPI.config.js'
 	import Header from '../components/common/Header.vue'
 	import Footer from '../components/common/Footer.vue'
+	import NotFound from '../components/common/NotFound.vue'
 	export default {
 		name: 'UserCollection',
+		data: function() {
+			return {
+				myAccount: {
+					user_id: '',
+					username: '',
+					email: '',
+					admin: ''
+				},
+				account: {
+					user_id: '',
+					username: '',
+					email: '',
+					admin: ''
+				},
+				pageNotFound: false
+			}
+		},
 		components: {
 			Header,
-			Footer
+			Footer,
+			NotFound
 		},
-		created: {
-
+		methods: {
+			getAccountsInfo() {
+				// if there is no user id in query -> 404
+				if (this.$route.query.id === undefined){
+					this.pageNotFound = true
+					return
+				}
+				// get page owner's info
+				let userID = this.$route.query.id
+				this.axios({
+					method: 'get',
+					url: `${API_URL}/user/${userID}/detail`,
+				}).then((res) => {
+					this.account = res.data
+				}).catch((error) => {
+					alert(error.response.data.message)
+					this.pageNotFound = true
+					return
+				})
+				// get my info (if exists)
+				if(this.$store.state.token){
+					this.axios({
+						method: 'get',
+						url: `${API_URL}/user/detail`,
+						headers: {
+							'Content-Type': 'application/json',
+							'AUTH-TOKEN': this.$store.state.token
+						}
+					}).then((res) => {
+						this.myAccount = res.data
+					}).catch((error) => {
+						this.pageNotFound = true
+						return
+					})
+				}
+			},
+			isMyDashboard(){
+				return this.myAccount.user_id === this.account.user_id ? true : false
+			},
+		},
+		mounted: function() {
+			this.getAccountsInfo()
 		}
 	}
 </script>
