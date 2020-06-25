011af9ccce7b1e5b465b741b4d77b87f90e7f603
YefengNiu <santece.rise@gmail.com>
2020-06-26 07:07:29 +0800

    Frontend: Modified api-url to localhost

--- frontend/rrp/src/serviceAPI.config.js
+++ frontend/rrp/src/serviceAPI.config.js
@@ -1,3 +1,3 @@
-// const API_URL = 'http://127.0.0.1:5000';
-const API_URL = 'http://8.210.91.242:5000';
+const API_URL = 'http://127.0.0.1:5000';
+// const API_URL = 'http://8.210.91.242:5000';
 export default API_URL;
