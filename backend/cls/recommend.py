5ab5ed076191e47552ec5af86c0c969d5bf52a01
Haozhe Chen <chz_chen@outlook.com>
2020-07-24 16:28:13 +0800

    Recommend by published_Date

--- backend/cls/recommend.py
+++ backend/cls/recommend.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 from pandas import read_sql
 
 from cls.book import Book
@@ -23,7 +24,7 @@ class Recommend:
     def get_recommend_list():
         Recommend.create_view_tables()
         conn = connect_sys_db()
-        query = 'select distinct books.id, books.categories,books.authors, view4.collect_time, view2.read_time, view3.avg_rating, books.google_rating from books left join collects on books.id = collects.book_id left join review_rate on books.id = review_rate.book_id left join view4 on books.id = view4.id left join view2 on books.id = view2.id left join view3 on books.id = view3.id'
+        query = 'select distinct books.id, books.categories,books.authors, books.published_date, view4.collect_time, view2.read_time, view3.avg_rating, books.google_rating from books left join collects on books.id = collects.book_id left join review_rate on books.id = review_rate.book_id left join view4 on books.id = view4.id left join view2 on books.id = view2.id left join view3 on books.id = view3.id'
         db_result = read_sql(sql=query, con=conn)
         # for item in db_result.iterrows():
         #     # print(item[1]['collect_time'])
@@ -34,6 +35,8 @@ class Recommend:
         #     if item[1]['avg_rating'].isnull():
         #         item[1]['avg_rating'] = 0
         db_result = db_result.fillna(0)
+        db_result['published_date'] = pd.to_datetime(db_result['published_date'])
+        db_result['published_date'] = db_result['published_date'].dt.strftime("%Y")
         db_result.insert(6, 'popular',
                          db_result.collect_time * 0.1 + db_result.read_time * 0.2 + db_result.avg_rating * 0.3 + db_result.google_rating * 0.4)
         # print(db_result.sort_values(by=['popular','categories'],ascending=False))
@@ -72,10 +75,31 @@ class Recommend:
                 book = Book(item[1]['id'])
                 info = book.get_info()
                 temp = {'book_id': item[1]['id'], 'author': item[1]['authors'], 'categories': item[1]['categories'],
-                        'popular': item[1]['popular'],'title': info.title, 'book_cover_url': info.book_cover_url}
+                        'popular': item[1]['popular'], 'title': info.title, 'book_cover_url': info.book_cover_url}
                 result.append(temp)
                 counter += 1
         return result
+
+    @staticmethod
+    def recommend_by_publishedDate(publishedDate, number, book_id):
+        df = Recommend.get_recommend_list()
+        # df = df.sort_values(by=['published_date', 'popular'], ascending=False)
+        df = df[df['published_date'].str.contains(publishedDate)]
+        df = df.sort_values(by=['popular'], ascending=False)
+        counter = 0
+        result = []
+        for item in df.iterrows():
+            if counter == number: break
+            if item[1]['id'] != book_id:
+                book = Book(item[1]['id'])
+                info = book.get_info()
+                temp = {'book_id': item[1]['id'], 'author': item[1]['authors'], 'categories': item[1]['categories'],
+                        'popular': item[1]['popular'], 'title': info.title, 'book_cover_url': info.book_cover_url}
+                result.append(temp)
+                counter += 1
+        return result
+        # print(df)
+        # df = df.sort_values(by=['authors', 'popular'], ascending=False)
 
     @staticmethod
     def recommend_by_author_category(category, author, number, book_id):
